ad80f5e5e81c7875777db4fb867203b022ece09e
Kantaro Kondo <kondo@route-zero.com>
2019-03-03 22:06:47 +0900

    変数を小文字に修正

--- RPG/src/Rpg.java
+++ RPG/src/Rpg.java
@@ -2,14 +2,14 @@
 public class Rpg {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		Person KANTARO = new Person("寛太郎",4900,29);
-		Person DAIJIRO = new Person("大二郎",38,1000);
-		Person KOTARO =  new Person("航太郎",38888,1);
-		
-		KANTARO.introduce();
-		DAIJIRO.introduce();
-		KOTARO.introduce();
+
+		Person kantaro = new Person("寛太郎",4900,29);
+		Person daijiro = new Person("大二郎",38,1000);
+		Person kotaro =  new Person("航太郎",38888,1);
+
+		kantaro.introduce();
+		daijiro.introduce();
+		kotaro.introduce();
 	}
 
 }
